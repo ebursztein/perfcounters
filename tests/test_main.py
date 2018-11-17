01775246bdade768c64fd44caf656fcae264f51c
Elie <git@elie.net>
2018-11-17 09:54:14 -0800

    Fixing unittests and coverage analysis

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,19 +1,23 @@
 import pytest
 import time
 import json
-from PerfCounters import PerfCounters
+from perfcounters import PerfCounters
+
 
 @pytest.fixture()
 def counters():
     counters = PerfCounters()
     return counters
 
+
 def test_set_counter(counters):
     counters.set('test', 42)
     assert counters.get('test') == 42
 
+
 def test_get_empty_counter(counters):
-    assert counters.get('test') == None
+    assert not counters.get('test')
+
 
 def test_time_counters(counters):
     counters.start('test')
@@ -21,18 +25,21 @@ def test_time_counters(counters):
     counters.stop('test')
     assert counters.get('test') > 1 and counters.get('test') < 2
 
+
 def test_error_on_non_started_counters(counters):
     with pytest.raises(ValueError):
         counters.stop('error')
+
 
 def test_to_json_values(counters):
     counters.set('cnt1', 42)
     js = counters.to_json()
     cnts = json.loads(js)
-    assert 'Timing counters' not in cnts    
+    assert 'Timing counters' not in cnts
     assert 'Value counters' in cnts
     assert cnts['Value counters'][0][0] == 'cnt1'
     assert cnts['Value counters'][0][1] == 42
+
 
 def test_to_json_time(counters):
     counters.start('tmt1')
@@ -40,5 +47,5 @@ def test_to_json_time(counters):
     js = counters.to_json()
     cnts = json.loads(js)
     assert 'Value counters' not in cnts
-    assert 'Timing counters' in cnts    
+    assert 'Timing counters' in cnts
     assert cnts['Timing counters'][0][0] == 'tmt1'
